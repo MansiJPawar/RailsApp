76859d3272f85afb5a580a50946e65e4454f28d4
MansiJPawar <mansipawar517@gmail.com>
2022-03-31 01:39:33 +0530

    devise gem performed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  devise_for :users
+
   root 'homes#index'
+
+  get 'pages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
