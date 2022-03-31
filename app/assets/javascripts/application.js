9075f51a0fd36fa87e51df6b474d361e7c94df04
MansiJPawar <mansipawar517@gmail.com>
2022-04-01 00:33:16 +0530

    Home page for user theme done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require activestorage
 //= require turbolinks
 //= require bootstrap.min
+//= require landingpage
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+ 
+  #home page
   root 'home#index'
+
+  # root 'profiles#index'
+
+  get 'profiles/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
