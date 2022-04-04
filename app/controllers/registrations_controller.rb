e89980b0278bf6b79e46e9a411d371bc5e2d44b9
MansiJPawar <mansipawar517@gmail.com>
2022-04-04 10:59:30 +0530

    removed unnecessary columns from devise params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,40 @@
 class RegistrationsController < ApplicationController
-  #to create new account 
-  def new 
-    #instance var to be visible in views
-    @account = Account.new
-  end 
+  before_action :configuration_sign_up_params, only: [:create]
+  before_action :configuration_account_update_params, only: [:update]
 
-  def create
-    @account =  Account.new(account_params)
-    if @account.save!
-      #session cookies encrypted store 
-      session[:account_id] = @account.id
-      redirect_to root_path, notice: "Account created"
-    else
-      render :new 
-    end 
+  def create 
+    super
   end
 
-  private
-
-  def account_params 
-    params.require(:account).permit(:email, :password, :password_confirmation)
+  def edit 
+    super
   end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:age, :gender, :phone_no, :image, :address, :name])
+      # devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+  end
+  # #to create new account 
+  # def new 
+  #   #instance var to be visible in views
+  #   @account = Account.new
+  # end 
+
+  # def create
+  #   @account =  Account.new(account_params)
+  #   if @account.save!
+  #     #session cookies encrypted store 
+  #     session[:account_id] = @account.id
+  #     redirect_to root_path, notice: "Account created"
+  #   else
+  #     render :new 
+  #   end 
+  # end
+
+  # private
+
+  # def account_params 
+  #   params.require(:account).permit(:email, :password, :password_confirmation)
+  # end
 
 end
